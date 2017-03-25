36e54fd19dc68b31fc8752305825375c732aac48
Esaú <esau.opr@gmail.com>
2017-03-24 23:39:00 -0600

    feact(utils): Add app token generator

---
+++ utils/tokenGenerator.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const Logger = require('./logger');
+
+const logger = new Logger;
+
+crypto.randomBytes(48, (error, buffer) => {
+    if (error) {
+        logger.error(error);
+        return;
+    }
+
+    const token = buffer.toString('hex');
+
+    fs.appendFile(`${__dirname}/../.env`, `\nAPP_TOKEN=${token}\n`, (error) => {
+        if (error) {
+            logger.error(error);
+            return;
+        }
+        logger.info(`Token generated: ${token}`);
+    });
+});
