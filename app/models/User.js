7b941e4337eed020ccb1d1bae4d00e1bee26b4e7
Irvin Castellanos <irvin.py@gmail.com>
2017-03-27 00:00:06 -0600

    User Schema

---
+++ app/models/User.js
@@ -0,0 +1,27 @@
+/*
+{
+	name: String,
+	lastname: String,
+	email: String,
+	softDeleted: Boolean,
+	password: String,
+	verificationToken: String,
+	isActive: Boolean,
+	createdAt: timestamp,
+	updatedAt: timestamp,
+	deletedAt: timestamp,
+	socialAccounts: [
+		{
+			providerUserId: String,
+			providerId: String
+		}
+	],
+	judgeUsers: [
+		{
+			userId: String,
+			username: String,
+			judgeId: String,
+		}
+	]
+} 
+*/
