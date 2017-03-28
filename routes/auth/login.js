12607251f902bb55362097f41bec95a08b0cfad1
Esaú <esau.opr@gmail.com>
2017-03-28 01:07:28 -0600

    refactor(routes): Refactor login code to a single file 'auth'

--- routes/auth/login.js
+++
@@ -1,9 +0,0 @@
-import LoginController from '../../app/http/controllers/auth/loginController';
-import auth from '../../app/http/middlewares/auth.js';
-
-function routeSetter(router) {
-    router.post('/login', LoginController.login);
-    router.get('/protected', auth, LoginController.functionProtected);
-}
-
-module.exports = routeSetter;
