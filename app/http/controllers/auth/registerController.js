68fa9dca97aa2fcd50f239fea2a2287b06f340b1
Esaú <esau.opr@gmail.com>
2017-03-28 01:01:35 -0600

    feact(controllers): Add register controller

---
+++ app/http/controllers/auth/registerController.js
@@ -0,0 +1,32 @@
+import User from '../../../models/user';
+import sha256 from '../../../../utils/sha256';
+
+async function register(ctx) {
+    try {
+        let user = new User({
+            name: ctx.request.body.name,
+            lastname: ctx.request.body.lastname,
+            email: ctx.request.body.email,
+            password: sha256(ctx.request.body.password)
+        });
+
+        await user.save();
+
+        user = JSON.parse(JSON.stringify(user));
+        delete user.password;
+
+        ctx.status = 201;
+        ctx.message = 'User was registered';
+        ctx.body = {
+            data: {
+                user: user
+            }
+        };
+    } catch (e) {
+        ctx.throw(422, e);
+    }
+}
+
+module.exports = {
+    register
+};
