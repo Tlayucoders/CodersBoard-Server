fb51fa58d7c02e8f518127e45c54af95bcb3116a
Esaú <esau.opr@gmail.com>
2017-03-28 00:54:42 -0600

    fix(middlewares): Fix async code execution

--- app/http/middlewares/auth.js
+++ app/http/middlewares/auth.js
@@ -3,14 +3,16 @@ import jwt from 'jsonwebtoken';
 /**
  * Middlewares for jwt authentication
  */
-module.exports = (ctx, next) => {
+async function auth(ctx, next) {
     const access_token = ctx.request.body.access_token || ctx.headers['x-access-token'] || '';
 
     try {
         jwt.verify(access_token, process.env.APP_TOKEN);
-        next();
+        await next();
     } catch(err) {
         ctx.status = 403;
         ctx.message = 'Forbidden resource';
     }
-};
+}
+
+module.exports = auth;
