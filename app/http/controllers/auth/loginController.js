62ae5ddedce22099e245dbe8030544f03e3792a6
Esaú <esau.opr@gmail.com>
2017-03-28 00:53:34 -0600

    feact(controllers): Add logic to user login

--- app/http/controllers/auth/loginController.js
+++ app/http/controllers/auth/loginController.js
@@ -1,21 +1,31 @@
 import jwt from 'jsonwebtoken';
+import User from '../../../models/user';
+import sha256 from '../../../../utils/sha256';
 
-function login(ctx) {
-    ctx.body = {
-        access_token: jwt.sign({
-            user: 'esau'
-        }, process.env.APP_TOKEN, { expiresIn: '7d' })
-    };
-    ctx.message = 'User logged';
-}
+/**
+ * Login
+ */
+async function login(ctx) {
+    try {
+        const user = await User.where('email', ctx.request.body.email)
+            .where('password', sha256(ctx.request.body.password))
+            .find();
 
-function functionProtected(ctx) {
-    ctx.body = {
-        data: [1,2,3,4]
-    };
+        if (!user.length) {
+            throw new Error('Email or password invalid');
+        }
+
+        ctx.body = {
+            access_token: jwt.sign({
+                user: JSON.parse(JSON.stringify(user))
+            }, process.env.APP_TOKEN, { expiresIn: '7d' })
+        };
+        ctx.message = 'User logged';
+    } catch (e) {
+        ctx.throw(404, e);
+    }
 }
 
 module.exports = {
-    login,
-    functionProtected
+    login
 };
