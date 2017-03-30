d9dcccd6cfafa3a6e03660b5b196b7e3403878ac
Esaú <esau.opr@gmail.com>
2017-03-30 00:10:38 -0600

    feat(api-v1-controllers): Add API annotations for users

--- app/http/controllers/api/v1/userController.js
+++ app/http/controllers/api/v1/userController.js
@@ -1,5 +1,41 @@
 import User from '../../../../models/user';
 
+/**
+ * @api {get} /users Request Users information
+ * @apiName GetUsera
+ * @apiGroup User
+ *
+ * @apiSuccess  {Object[]}  Users               List fo users
+ * æapiSuccess  {String}    Users._id           User._id
+ * @apiSuccess  {String}    Users.name          Name of the User
+ * @apiSuccess  {String}    Users.lastname      lastname of the User
+ * @apiSuccess  {String}    Users.created_at    Date of user registration
+ * @apiSuccess  {String}    Users.updated_at    Date of last user information update
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "data": {
+ *
+        	   "users": [
+        			{
+        				"_id": "58d8b70c4a9f312d045bff35",
+        				"name": "Esaú",
+        				"lastname": "Peralta",
+        				"created_at": "2017-03-27T06:54:04.318Z",
+        				"updated_at": "2017-03-27T06:54:04.318Z"
+        			}
+                ]
+ *          }
+ *     }
+ *
+ * @apiError UsersNotFound The was a error getting the users information
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "Message error"
+ *     }
+ */
 async function fetch(ctx) {
     try {
         const users = await User.exclude('password').find();
