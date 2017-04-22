03b52c14768577e75146ad2846bb2dcd441c2faa
Esaú Peralta <esau.opr@gmail.com>
2017-04-22 01:59:53 -0500

    feat(routes): Add route to link a judge account

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
 import LoginController from '../../app/http/controllers/auth/loginController';
 import RegisterController from '../../app/http/controllers/auth/registerController';
+import auth from '../../app/http/middlewares/auth';
 
 function routeSetter(router) {
     router.post('/login', LoginController.login);
     router.post('/register', RegisterController.register);
+    router.post('/register/link', auth, RegisterController.accountLink);
 }
 
 module.exports = routeSetter;
