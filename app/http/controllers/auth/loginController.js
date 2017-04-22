1e34331398a3b8e4eab8a394736aecea3af1a449
Esaú Peralta <esau.opr@gmail.com>
2017-04-22 02:02:52 -0500

    feat(controllers): Add comments to generate apidoc and change mongo query find to finOne

--- app/http/controllers/auth/loginController.js
+++ app/http/controllers/auth/loginController.js
@@ -3,7 +3,27 @@ import User from '../../../models/user';
 import sha256 from '../../../../utils/sha256';
 
 /**
- * Login
+ * @api {post} /login User Login
+ * @apiName UserLogin
+ * @apiGroup User
+ *
+ * @apiSuccess  {Object}    data                Response data
+ * @apiSuccess  {String}    data.access_token  User access token
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "data": {
+ *        	   "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJ1c2VyIj"
+ *          }
+ *     }
+ *
+ * @apiError NOTFOUND Invalid user information
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "Message error"
+ *     }
  */
 async function login(ctx) {
     try {
@@ -12,16 +32,18 @@ async function login(ctx) {
             .exclude('updated_at')
             .where('email', ctx.request.body.email)
             .where('password', sha256(ctx.request.body.password))
-            .find();
+            .findOne();
 
-        if (!user.length) {
+        if (!user) {
             throw new Error('Email or password invalid');
         }
 
         ctx.body = {
-            access_token: jwt.sign({
-                user: JSON.parse(JSON.stringify(user))
-            }, process.env.APP_TOKEN, { expiresIn: '7d' })
+            data: {
+                access_token: jwt.sign({
+                    user: user.toJSON()
+                }, process.env.APP_TOKEN, { expiresIn: '7d' })
+            }
         };
         ctx.message = 'User logged';
     } catch (e) {
