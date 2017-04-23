0a55265420b4d3b0591c8f8e37c2acda09873799
Esaú Peralta <esau.opr@gmail.com>
2017-04-22 22:18:02 -0500

    feat(controllers): Add judge controller

---
+++ app/http/controllers/api/v1/judgeController.js
@@ -0,0 +1,52 @@
+import Judge from '../../../../models/judge';
+
+/**
+ * @api {get} /api/v1/judges Request judges information
+ * @apiName GetJudges
+ * @apiGroup Judge
+ *
+ * @apiSuccess  {Object}    data                Response data
+ * @apiSuccess  {Object[]}  data.judges         List fo judges
+ * @apiSuccess  {String}    data.judges._id     Judge id
+ * @apiSuccess  {String}    data.judges.name    Judge name
+ * @apiSuccess  {String}    data.judges.url     Judge lastname
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "data": {
+ *        	   "judges": [
+ *        			{
+ *        				"_id": "58d8b70c4a9f312d045bff35",
+ *        				"name": "Uva Online Judge",
+ *        				"url": "https://uva.onlinejudge.org/",
+ *        			}
+ *              ]
+ *          }
+ *     }
+ *
+ * @apiError JudgesNotFound The was a error getting the judges information
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "Message error"
+ *     }
+ */
+async function fetch(ctx) {
+    try {
+        const judges = await Judge.exclude('created_at')
+            .exclude('updated_at').find();
+        ctx.body = {
+            data: {
+                judges: JSON.parse(JSON.stringify(judges))
+            }
+        };
+    } catch (e) {
+        ctx.logger.info(e);
+        ctx.throw(404, e);
+    }
+}
+
+module.exports = {
+    fetch
+};
