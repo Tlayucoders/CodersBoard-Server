a9219a0ca9258541fe5266786516ec0e89d39c3b
Esaú Peralta <esau.opr@gmail.com>
2017-04-22 22:15:43 -0500

    feat(routes): Add route to get judges

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,4 +1,5 @@
 import UserController from '../../app/http/controllers/api/v1/userController';
+import JudgeController from '../../app/http/controllers/api/v1/judgeController';
 import auth from '../../app/http/middlewares/auth';
 
 const API_PREFIX = '/api/v1';
@@ -6,6 +7,8 @@ const API_PREFIX = '/api/v1';
 function routeSetter(router) {
     // Users
     router.get(`${API_PREFIX}/users`, auth, UserController.fetch);
+    // judges
+    router.get(`${API_PREFIX}/judges`, auth, JudgeController.fetch);
 }
 
 module.exports = routeSetter;
