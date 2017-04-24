548b3bb03a27824bcb4ebebfa919e5809b3543ea
Esaú Peralta <esau.opr@gmail.com>
2017-04-23 23:08:29 -0500

    refactor(models): Change ajv by joi to validations

--- app/models/hub.js
+++ app/models/hub.js
@@ -12,26 +12,49 @@ const logger = new Logger();
 class Jub extends Model {
     configure () {
         this.setSchema();
-        // this.before('create', 'validate');
-        // this.before('update', 'validate');
         this.before('save', 'validate');
+        this.before('create', 'checkIfExists');
     }
 
+    /**
+     * Model schema used to validations
+     */
     setSchema () {
         this.schema = Joi.object().keys({
-            name: Joi.string().alphanum().min(3).max(30).required(),
+            _id: Joi.string(),
+            unique_key: Joi.string(),
+            name: Joi.string().min(3).max(30).required(),
             description: Joi.string().min(3),
             created_at: Joi.date(),
             updated_at: Joi.date()
         });
     }
 
+    /**
+     * Validate the record attributes throw a exception if is invalid
+     * @param {function} next callback function
+     */
     async validate(next) {
-        const ans = Joi.validate(JSON.parse(JSON.stringify(this.attributes)), this.schema);
-
+        const ans = Joi.validate(JSON.parse(JSON.stringify(this.attributes)), this.schema, {
+            convert: false
+        });
         if (ans.error) {
             logger.warn(ans.error.toString());
             throw new Error(ans.error.toString());
+        }
+        await next;
+    }
+
+    /**
+     * Validate if the record exists throw a exception if this exists
+     * @param {function} next callback function
+     */
+    async checkIfExists(next) {
+        const hub = await Jub.where('unique_key', this.attributes.unique_key).findOne();
+        if (hub) {
+            const error = 'ValidationError:: The hub is already registred';
+            logger.error(error);
+            throw new Error(error);
         }
         await next;
     }
--- app/models/judge.js
+++ app/models/judge.js
@@ -11,29 +11,38 @@ const logger = new Logger();
 class Judge extends Model {
     configure () {
         this.setSchema();
-        // this.before('create', 'validate');
-        // this.before('update', 'validate');
         this.before('save', 'validate');
     }
 
+    /**
+     * Model schema used to validations
+     */
     setSchema () {
         this.schema = Joi.object().keys({
-            name: Joi.string().trim().alphanum().min(3).max(30).required(),
+            _id: Joi.string(),
+            name: Joi.string().trim().min(3).max(30).required(),
             url: Joi.string().trim().uri(),
             created_at: Joi.date(),
             updated_at: Joi.date()
         });
     }
 
+    /**
+     * Validate the record attributes throw a exception if is invalid
+     * @param {function} next callback function
+     */
     async validate(next) {
-        const valid = ajv.validate(this.schema, JSON.parse(JSON.stringify(this.attributes)));
-
-        if (!valid) {
-            logger.warn(ajv.errorsText());
-            throw new Error(ajv.errorsText());
+        const ans = Joi.validate(JSON.parse(JSON.stringify(this.attributes)), this.schema, {
+            convert: false
+        });
+        if (ans.error) {
+            logger.warn(ans.error.toString());
+            throw new Error(ans.error.toString());
         }
         await next;
     }
+
+
 }
 
 
