ae77a1c1b496c5d61740aaf546483933bb6f72dc
Esaú Peralta <esau.opr@gmail.com>
2017-04-23 19:30:13 -0500

    refactor(routes): Change judge account vinculation to api v1 routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,5 @@
 import LoginController from '../../app/http/controllers/auth/loginController';
 import RegisterController from '../../app/http/controllers/auth/registerController';
-import auth from '../../app/http/middlewares/auth';
 
 /**
  * Set the routes defined inside
@@ -9,7 +8,6 @@ import auth from '../../app/http/middlewares/auth';
 function routeSetter(router) {
     router.post('/login', LoginController.login);
     router.post('/register', RegisterController.register);
-    router.post('/register/link', auth, RegisterController.accountLink);
 }
 
 module.exports = routeSetter;
